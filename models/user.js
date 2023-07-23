44e947cb5d36ca9573a2f38e63b8dabe8744b569
AnandKumar <anandkumar031199@gmail.com>
2023-07-23 20:51:20 +0530

    logic written

--- models/user.js
+++ models/user.js
@@ -1,5 +1,24 @@
-var mongoose= require("mongoose");
+const { string } = require("joi");
+var mongoose = require("mongoose");
 
 //Write your schema here with name of schema as userSchema
+userSchema = {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  pswd: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["buyer", "seller"],
+  },
+};
 
-module.exports =mongoose.model("users",userSchema);
+module.exports = mongoose.model("users", userSchema);
